5cb40778117f61c9fe1064448ff6cb2384183a26
jdy7149 <184757480+jdy7149@users.noreply.github.com>
2025-11-06 21:15:39 +0900

    End implementation of tree

--- Tree.js
+++ Tree.js
@@ -6,69 +6,69 @@ export default class Tree {
   }
 
   insert(value) {
-    const helper = (node) => {
+    const insertNode = (node) => {
       if (!node) {
         return new Node(value);
       }
 
       if (value < node.value) {
-        node.left = helper(node.left);
+        node.left = insertNode(node.left);
       } else if (value > node.value) {
-        node.right = helper(node.right);
+        node.right = insertNode(node.right);
       } else {
         throw Error('Duplication not allowed!')
       }
     };
 
-    this.root = helper(this.root);
+    this.root = insertNode(this.root);
   }
 
   deleteItem(value) {
-    const helper = (node, value) => {
+    const deleteNode = (node, target) => {
       if (!node) return null;
 
-      if (value === node.value) {
+      if (target === node.value) {
         if (node.left && node.right) {
-          let nextBiggest = node.right;
+          let successor = node.right;
 
-          while (nextBiggest.left) {
-            nextBiggest = nextBiggest.left;
+          while (successor.left) {
+            successor = successor.left;
           }
 
-          node.value = nextBiggest.value;
+          node.value = successor.value;
 
-          node.right = helper(node.right, nextBiggest.value);
+          node.right = deleteNode(node.right, successor.value);
         } else {
           return node.left ?? node.right;
         }
-      } else if (value < node.value) {
-        node.left = helper(node.left, value);
+      } else if (target < node.value) {
+        node.left = deleteNode(node.left, target);
       } else {
-        node.right = helper(node.right, value);
+        node.right = deleteNode(node.right, target);
       }
 
       return node;
     };
 
-    this.root = helper(this.root, value);
+    this.root = deleteNode(this.root, value);
   }
 
   find(value) {
-    const helper = (node) => {
+    const findNode = (node) => {
       if (!node) {
         return null;
       }
       
       if (value < node.value) {
-        return helper(node.left);
-      } else if (value > node.right) {
-        return helper(node.right);
+        return findNode(node.left);
+      } else if (value > node.value) {
+        return findNode(node.right);
       } else {
         return node;
       }
     };
 
-    return helper(this.root);
+    return findNode(this.root);
   }
 
   levelOrderForEach(callback) {
@@ -91,93 +91,105 @@ export default class Tree {
   }
 
   inOrderForEach(callback) {
-    const inOrderTraverse = (root) => {
+    const traverseInOrder = (root) => {
       if (!root) {
         return;
       }
 
-      inOrderTraverse(root.left);
+      traverseInOrder(root.left);
       callback(root);
-      inOrderTraverse(root.right);
+      traverseInOrder(root.right);
     };
 
-    inOrderTraverse(this.root);
+    traverseInOrder(this.root);
   }
 
   preOrderForEach(callback) {
-    const preOrderTraverse = (root) => {
+    const traversePreOrder = (root) => {
       if (!root) {
         return;
       }
 
       callback(root);
-      preOrderTraverse(root.left);
-      preOrderTraverse(root.right);
+      traversePreOrder(root.left);
+      traversePreOrder(root.right);
     };
 
-    preOrderTraverse(this.root);
+    traversePreOrder(this.root);
   }
 
   postOrderForEach(callback) {
-    const postOrderTraverse = (root) => {
+    const traversePostOrder = (root) => {
       if (!root) {
         return;
       }
 
-      postOrderTraverse(root.left);
-      postOrderTraverse(root.right);
+      traversePostOrder(root.left);
+      traversePostOrder(root.right);
       callback(root);
     };
 
-    postOrderTraverse(this.root);
+    traversePostOrder(this.root);
   }
 
   height(value) {
-    const helper = (node, h) => {
+    const findHeight = (node, h) => {
       if (!node) return h;
       
       if (h > -1 || value === node.value) {
-        return Math.max(helper(node.left, h + 1), helper(node.right, h + 1));
+        return Math.max(findHeight(node.left, h + 1), findHeight(node.right, h + 1));
       } else if (value < node.value) {
-        return helper(node.left, h);
+        return findHeight(node.left, h);
       } else {
-        return helper(node.right, h);
+        return findHeight(node.right, h);
       }
     };
 
-    const h = helper(this.root, -1);
-    return h === -1 ? null : h;
+    const result = findHeight(this.root, -1);
+    return result === -1 ? null : result;
   }
   
   depth(value) {
-    const helper = (node) => {
-      if (!node) {
-        return null;
-      }
-
-      if (value === node.value) {
-        return 0;
-      } 
+    const findDepth = (node) => {
+      if (!node) return null;
+      if (value === node.value) return 0;
 
       const next = value < node.value ? node.left : node.right;
-      const lower = helper(next);
+      const childDepth = findDepth(next);
 
-      return lower == null ? null : lower + 1;
+      return childDepth == null ? null : childDepth + 1;
     };
 
-    return helper(this.root);
+    return findDepth(this.root);
   }
 
   isBalanced() {
+    const check = (node) => {
+      if (!node) {
+        return {
+          balanced: true,
+          height: -1,
+        };
+      }
 
+      const { balanced: isLeftBalnced, height: leftHeight } = check(node.left);
+      const { balanced: isRightBalnced, height: rightHeight } = check(node.right);
+
+      return {
+        balanced: isLeftBalnced && isRightBalnced && Math.abs(leftHeight - rightHeight) <= 1,
+        height: Math.max(leftHeight, rightHeight) + 1,
+      };
+    };
+
+    return check(this.root).balanced;
   }
 
   rebalance() {
-    const nodes = [];
+    const sortedValues = [];
 
-    this.inOrderForEach((node) => nodes.push(node.value));
+    this.inOrderForEach((node) => sortedValues.push(node.value));
 
-    this.root = this.#buildTree(nodes);
+    this.root = this.#buildTree(sortedValues);
   }
 
   #buildTree(arr) {
